919a813ad866e156700dd92dbb63a86003a5cd50
hadi <aalgvs8@gmail.com>
2026-06-25 22:38:21 +0330

    init06

--- handlers/admin/manage_orders_admin.py
+++ handlers/admin/manage_orders_admin.py
@@ -7,9 +7,9 @@ order_router = Router()
 # ==========================
 # Orders Managing
 # ==========================
-@product_router_A.message(F.text == "Show Order 🛒")
-async def show_order(message: Message, state: FSMContext):
-    await message.answer("Enter Order ID")
-    await state.set_state(OrdersFSM.id)
+# @product_router_A.message(F.text == "Show Order 🛒")
+# async def show_order(message: Message, state: FSMContext):
+#     await message.answer("Enter Order ID")
+#     await state.set_state(OrdersFSM.id)
 
-@product_router_A
+# @product_router_A
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from aiogram import Dispatcher, Bot
 from config import BOT_TOKEN
 from routers import setup_routers
+from aiogram.client.telegram import TelegramAPIServer
 import asyncio
+from aiogram.client.session.aiohttp import AiohttpSession
 
-bot = Bot(BOT_TOKEN)
+api = TelegramAPIServer.from_base(
+    base="http://127.0.0.1:8081"
+)
+
+session = AiohttpSession(api=api)
+
+bot = Bot(token=BOT_TOKEN, session=session)
 dp = Dispatcher()
 
 setup_routers(dp)
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
     
 if __name__ == "__main__":
